69277167a0a295164eef5015af9fd5ec816d5e6d
Stanislav Stashchuk <stashchuk_stanislav@ukr.net>
2022-02-22 21:38:29 +0200

    path

--- netlify/functions/using-path.js
+++ netlify/functions/using-path.js
@@ -1,12 +1,38 @@
-const generateHtml = (name = 'there') => {
-    return `
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1>Hi ${name}</h1>
-      </body>
-    </html>`
-  }
+
+exports.handler = async (event, context) => {
+    const path = event.path.replace(/\/\.netlify\/functions\/[^/]*\//, '')
+    const pathParts = (path) ? path.split('/') : []
+    
+    console.log("event.path");
+    console.log(event.path);
+
+    console.log("path");
+    console.log(path);
+
+    console.log("pathParts");
+    console.log(pathParts);
   
+    const generateHtml = (name = 'there') => {
+      /** For security *always* escape output html
+        const safeValues = escapeHtml(name)
+      */
+      return `
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1>Hi ${name}</h1>
+        </body>
+      </html>`
+    }
+  
+    return {
+      'statusCode': 200,
+      'headers': {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'text/html',
+      },
+      'body': generateHtml(pathParts[0])
+    }
+  }
