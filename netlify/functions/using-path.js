c335c379e2bf6b1a2709f95260effb6afc1d96d3
Stanislav Stashchuk <stashchuk_stanislav@ukr.net>
2022-02-22 20:43:18 +0200

    commit

---
+++ netlify/functions/using-path.js
@@ -0,0 +1,12 @@
+const generateHtml = (name = 'there') => {
+    return `
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>Hi ${name}</h1>
+      </body>
+    </html>`
+  }
+  
